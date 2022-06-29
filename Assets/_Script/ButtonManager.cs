74012afbab01e3f77193c84434631b630f4da459
준1 <98933515+Wizone06@users.noreply.github.com>
2022-06-29 12:11:16 +0900

    end.
    end.

---
+++ Assets/_Script/ButtonManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject SulmeongPanel;
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+    public void Sulmeong()
+    {
+        SulmeongPanel.SetActive(true);
+    }
+}
